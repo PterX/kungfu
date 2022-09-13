d4693ad237b038ded9f9a2b903a5d8896e1e2c75
songzhouran <zhouran.song@kungfu>
2022-09-13 17:38:13 +0800

    [fix] live session read

--- framework/core/src/bindings/node/binding/journal.cpp
+++ framework/core/src/bindings/node/binding/journal.cpp
@@ -397,7 +397,7 @@ Napi::Value Assemble::Get_reader(const Napi::CallbackInfo &info) {
   auto node_group = Napi::String::New(info.Env(), sessions[index].group);
   auto node_name = Napi::String::New(info.Env(), sessions[index].name);
   auto begin_time = Napi::BigInt::New(info.Env(), t_begin > 0 ? t_begin : sessions[index].begin_time);
-  auto end_time = Napi::BigInt::New(info.Env(), t_end > 0 ? t_end : std::abs(sessions[index].end_time));
+  auto end_time = Napi::BigInt::New(info.Env(), t_end > 0 ? t_end : (sessions[index].end_time == 0 ? kungfu::yijinjing::time::now_in_nano() : std::abs(sessions[index].end_time)));
   auto reader = Reader::constructor.New({node_mode, node_category, node_group, node_name, begin_time, end_time});
   return reader;
 }

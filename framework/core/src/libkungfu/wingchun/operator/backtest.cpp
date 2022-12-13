9c807db4a04d97d3278f39e0add57976504f6812
Wei-Jianan <weij@tcd.ie>
2022-12-13 18:21:43 +0800

    operator::Context and operator::BacktestContext no implemented

---
+++ framework/core/src/libkungfu/wingchun/operator/backtest.cpp
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+
+//
+// Created by Wei Jianan on 2022-12-12.
+//
+
+#include <kungfu/wingchun/operator/backtest.h>
+
+namespace kungfu::wingchun::op {
+
+}
+

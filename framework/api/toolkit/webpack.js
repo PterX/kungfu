490458d75e6031c33d51a972e3a250701114d5fd
yizhi <zhangyiizhi@126.com>
2022-12-21 18:26:20 +0800

    [fix] remove sdk optional dependencies

--- framework/api/toolkit/webpack.js
+++ framework/api/toolkit/webpack.js
@@ -2,7 +2,6 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const { isProduction, getAppDir } = require('./utils');
-const webpack = require('webpack');
 
 module.exports = {
   makeConfig: (argv) => {
@@ -106,13 +105,7 @@ module.exports = {
           extensions: ['js', 'json', 'ts', 'css', 'less'],
           exclude: 'node_modules',
           failOnWarning: !production,
-        }),
-        new webpack.IgnorePlugin({
-          resourceRegExp: /kungfu-cli/,
-        }),
-        new webpack.IgnorePlugin({
-          resourceRegExp: /kungfu-app/,
-        }),
+        })
       ],
       resolve: {
         alias: {

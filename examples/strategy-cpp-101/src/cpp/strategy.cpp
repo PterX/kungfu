e93c1665e2bf2ee8eda649d018b26e8297179a95
kfkev <kevcqu@qq.com>
2023-02-16 11:31:26 +0800

    [add] Runner -a arguments and context arguments

--- examples/strategy-cpp-101/src/cpp/strategy.cpp
+++ examples/strategy-cpp-101/src/cpp/strategy.cpp
@@ -16,6 +16,7 @@ public:
 
   void pre_start(Context_ptr & context) override {
     SPDLOG_INFO("preparing strategy");
+    SPDLOG_INFO("arguments: {}", context->arguments());
     context->add_account("sim", "1");
     context->subscribe("sim", {"600000"}, {"SSE"});
     // context->subscribe_operator("bar", "my-bar");

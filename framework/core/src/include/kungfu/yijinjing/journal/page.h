e4e49d9fbb1246765e74faf9a7b3378d114ec84b
yizhi <zhangyiizhi@126.com>
2023-02-22 09:57:31 +0800

    [fix] limit category operator journal creation size == 16MB

--- framework/core/src/include/kungfu/yijinjing/journal/page.h
+++ framework/core/src/include/kungfu/yijinjing/journal/page.h
@@ -79,7 +79,8 @@ inline static uint32_t find_page_size(const data::location_ptr &location, uint32
     return 128 * MB;
   }
   if ((location->category == longfist::enums::category::TD ||
-       location->category == longfist::enums::category::STRATEGY) &&
+       location->category == longfist::enums::category::STRATEGY ||
+       location->category == longfist::enums::category::OPERATOR) &&
       dest_id != 0) {
     return 16 * MB;
   }

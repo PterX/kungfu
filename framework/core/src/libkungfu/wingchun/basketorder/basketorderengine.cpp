dbebed36747332744ed39b67b515bf7fd3accf60
yizhi <zhangyiizhi@126.com>
2023-03-01 12:39:21 +0800

    [fix] use condtion variable && add bus

--- framework/core/src/libkungfu/wingchun/basketorder/basketorderengine.cpp
+++ framework/core/src/libkungfu/wingchun/basketorder/basketorderengine.cpp
@@ -75,7 +75,6 @@ void BasketOrderEngine::update_basket_order(int64_t trigger_time, const longfist
 
 bool BasketOrderEngine::try_update_basket_order(int64_t trigger_time, const longfist::types::Order &order) {
   if (order.parent_id == (uint64_t)0) {
-    SPDLOG_DEBUG("not a basket order");
     return false;
   }
 

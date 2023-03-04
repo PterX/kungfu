76dc4d79c1be7e0ec898a4b5106ee849c68d9019
Wei-Jianan <weij@tcd.ie>
2023-03-04 14:14:07 +0800

    [fix] clang-format bug. add ; at the end of ctor fix it.

--- examples/data-tool/src/cpp/write_backtest_data.cpp
+++ examples/data-tool/src/cpp/write_backtest_data.cpp
@@ -6,55 +6,57 @@ using namespace kungfu::longfist::enums;
 using namespace kungfu::longfist::types;
 using namespace kungfu::yijinjing::data;
 using namespace kungfu::wingchun::tool;
+using namespace kungfu::yijinjing::journal;
 using kungfu::yijinjing::time;
 
-KUNGFU_MAIN_TOOL(TestSimDataTool){
-  public : TestSimDataTool(category category, std::string group, std::string name, int64_t start_time, int64_t end_time,
-                           locator_ptr locator, bool overwrite = true) :
-      CacheTool(category, group, name, start_time, end_time, locator, overwrite){}
+KUNGFU_MAIN_TOOL(TestSimDataTool) {
+public:
+  TestSimDataTool(category category, std::string group, std::string name, int64_t start_time, int64_t end_time,
+                  locator_ptr locator, bool overwrite = true)
+      : CacheTool(category, group, name, start_time, end_time, locator, overwrite){};
 
-  virtual void run() override{kungfu::yijinjing::journal::frame_ptr frame;
-for (std::size_t i = 0; i < 100; ++i) {
-  Quote quote{};
-  quote.instrument_id = "test";
-  quote.exchange_id = "SSE";
-  quote.bid_price[0] = i;
-  quote.ask_price[0] = i + 1;
-  quote.data_time = get_begin_time() + i;
-  write_at(quote.data_time, quote.data_time, location::PUBLIC, quote);
-  frame = current_frame();
-  spdlog::info("frame: {}", frame->data<Quote>().to_string());
-  assert(frame->msg_type() == Quote::tag);
-  assert(frame->data<Quote>().to_string() == quote.to_string());
-  next();
+  virtual void run() override {
+    frame_ptr frame;
+    for (std::size_t i = 0; i < 100; ++i) {
+      Quote quote{};
+      quote.instrument_id = "test";
+      quote.exchange_id = "SSE";
+      quote.bid_price[0] = i;
+      quote.ask_price[0] = i + 1;
+      quote.data_time = get_begin_time() + i;
+      write_at(quote.data_time, quote.data_time, location::PUBLIC, quote);
+      frame = current_frame();
+      spdlog::info("frame: {}", frame->data<Quote>().to_string());
+      assert(frame->msg_type() == Quote::tag);
+      assert(frame->data<Quote>().to_string() == quote.to_string());
+      next();
 
-  Entrust entrust{};
-  entrust.instrument_id = "test";
-  entrust.exchange_id = "SSE";
-  entrust.price = i;
-  entrust.data_time = get_begin_time() + i;
-  write_at(entrust.data_time, entrust.data_time, location::PUBLIC, entrust);
-  frame = current_frame();
-  spdlog::info("frame: {}", frame->data<Entrust>().to_string());
-  assert(frame->msg_type() == Entrust::tag);
-  assert(frame->data<Entrust>().to_string() == entrust.to_string());
-  next();
+      Entrust entrust{};
+      entrust.instrument_id = "test";
+      entrust.exchange_id = "SSE";
+      entrust.price = i;
+      entrust.data_time = get_begin_time() + i;
+      write_at(entrust.data_time, entrust.data_time, location::PUBLIC, entrust);
+      frame = current_frame();
+      spdlog::info("frame: {}", frame->data<Entrust>().to_string());
+      assert(frame->msg_type() == Entrust::tag);
+      assert(frame->data<Entrust>().to_string() == entrust.to_string());
+      next();
 
-  Transaction transaction{};
-  transaction.instrument_id = "test";
-  transaction.exchange_id = "SSE";
-  transaction.price = i;
-  transaction.data_time = get_begin_time() + i;
-  write_at(transaction.data_time, transaction.data_time, location::PUBLIC, transaction);
-  frame = current_frame();
-  spdlog::info("frame: {}", frame->data<Transaction>().to_string());
-  assert(frame->msg_type() == Transaction::tag);
-  assert(frame->data<Transaction>().to_string() == transaction.to_string());
-  next();
-}
-}
-}
-;
+      Transaction transaction{};
+      transaction.instrument_id = "test";
+      transaction.exchange_id = "SSE";
+      transaction.price = i;
+      transaction.data_time = get_begin_time() + i;
+      write_at(transaction.data_time, transaction.data_time, location::PUBLIC, transaction);
+      frame = current_frame();
+      spdlog::info("frame: {}", frame->data<Transaction>().to_string());
+      assert(frame->msg_type() == Transaction::tag);
+      assert(frame->data<Transaction>().to_string() == transaction.to_string());
+      next();
+    }
+  }
+};
 
 int another_usage_example() {
   auto backtest_locator = std::make_shared<locator>("/home/wei/.config/kungfu/home/backtest");

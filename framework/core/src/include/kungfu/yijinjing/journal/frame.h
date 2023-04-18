3514203c8d1636a841c54d0fa021f7ee5490288b
yizhi <zhangyiizhi@126.com>
2023-04-18 11:06:34 +0800

    [fix] archive sink

--- framework/core/src/include/kungfu/yijinjing/journal/frame.h
+++ framework/core/src/include/kungfu/yijinjing/journal/frame.h
@@ -96,7 +96,6 @@ private:
   void set_dest(uint32_t dest) { header_->dest = dest; }
 
   void copy(frame &source) {
-    memset(header_, 0, source.frame_length());
     memcpy(header_, source.header_, source.frame_length());
   }
 

02fa375a601add82d63d369992d0617b44aa9688
kfkev <kevcqu@qq.com>
2023-04-28 14:42:43 +0800

    [delete] xtp test code

--- extensions/xtp/src/cpp/trader_xtp.cpp
+++ extensions/xtp/src/cpp/trader_xtp.cpp
@@ -41,27 +41,6 @@ TraderXTP::~TraderXTP() {
 }
 
 void TraderXTP::on_start() {
-  std::vector<std::thread> threads{};
-  for (int i = 0; i < 32; ++i) {
-    threads.push_back(std::thread([&]() {
-      int j = 1e4;
-      while (--j > 0) {
-        auto &data = get_writer(location::PUBLIC)->open_data<HistoryOrder>();
-        data.order_id = get_writer(location::PUBLIC)->current_frame_uid();
-        get_writer(location::PUBLIC)->close_data();
-        SPDLOG_DEBUG("HistoryOrder: {}", data.to_string());
-        if (i % 2 == 0) {
-          std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        }
-      }
-    }));
-  }
-
-  for (auto &t : threads) {
-    t.join();
-    SPDLOG_WARN("join: {}", t.get_id());
-  }
-
   TDConfiguration config = nlohmann::json::parse(get_config());
   if (config.client_id < 1 or config.client_id > 99) {
     SPDLOG_ERROR("client_id must between 1 and 99");

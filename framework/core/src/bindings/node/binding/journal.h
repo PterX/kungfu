027a29e2f3f48ffcc5722028a4260259170c397d
yizhi <zhangyiizhi@126.com>
2023-05-06 10:28:15 +0800

    [add] frame data_to_string in node

--- framework/core/src/bindings/node/binding/journal.h
+++ framework/core/src/bindings/node/binding/journal.h
@@ -62,6 +62,8 @@ public:
 
   Napi::Value Data(const Napi::CallbackInfo &info);
 
+  Napi::Value DataToString(const Napi::CallbackInfo &info);
+
   static void Init(Napi::Env env, Napi::Object exports);
 
   static Napi::Value NewInstance(Napi::Value arg);

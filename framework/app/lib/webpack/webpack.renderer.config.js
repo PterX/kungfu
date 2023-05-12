07374710bbc007391ab6807ad5ec2b6498a1bc5f
yizhi <zhangyiizhi@126.com>
2023-05-12 10:37:37 +0800

    [add] generate code key words on compiler

--- framework/app/lib/webpack/webpack.renderer.config.js
+++ framework/app/lib/webpack/webpack.renderer.config.js
@@ -19,11 +19,17 @@ const {
 } = require('@kungfu-trader/kungfu-js-api/toolkit/utils');
 const CopyPlugin = require('copy-webpack-plugin');
 const fse = require('fs-extra');
+const {
+  generateCodeKeyWords,
+} = require('@kungfu-trader/kungfu-js-api/toolkit/generateCodeKeyWords');
 
 const appDir = getAppDir();
 const { getKungfuBuildInfo, getPagesConfig, isProduction } = toolkit.utils;
 const { pyVersion } = getKungfuBuildInfo();
 const publicDir = path.join(appDir, 'public');
+
+
+generateCodeKeyWords();
 
 const webpackConfig = (argv) => {
   const pagesConfig = getPagesConfig(argv);

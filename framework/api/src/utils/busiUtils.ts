ea33889afe8f96ef459d066fa24983c8238be7a8
liujiaecit <liujiaecit@163.com>
2023-05-16 10:50:09 +0800

    [fix] replaceNonAlphaNumericWithSpace filter type

--- framework/api/src/utils/busiUtils.ts
+++ framework/api/src/utils/busiUtils.ts
@@ -1236,7 +1236,7 @@ export const getConfigValue = (kfConfig: KungfuApi.KfConfig) => {
   return JSON.parse(kfConfig.value || '{}');
 };
 
-export const buildIdByKeysFromKfConfigSettings = (
+export const buildIdByPrimaryKeysFromKfConfigSettings = (
   kfConfigState: Record<string, KungfuApi.KfConfigValue>,
   keys: string[],
 ) => {
@@ -1941,8 +1941,14 @@ export const getPrimaryKeys = (
 ): string[] => {
   return settings.filter((item) => item.primary).map((item) => item.key);
 };
-export const replaceNonAlphaNumericWithSpace = (str: string) => {
-  return str.replace(/[^a-zA-Z0-9]+/g, '');
+export const replaceNonAlphaNumericWithSpace = (
+  value: KungfuApi.KfConfigValue,
+) => {
+  if (typeof value === 'string') {
+    return value.replace(/[^a-zA-Z0-9]+/g, '');
+  } else {
+    return value;
+  }
 };
 const concatPrimaryKey = (arr: string[]) => {
   if (arr.length === 0) return '';

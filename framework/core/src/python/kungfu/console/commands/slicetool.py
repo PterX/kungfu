814d00f7bbd57605cf1ee6ca9b60d804e4177a43
Wei-Jianan <weij@tcd.ie>
2023-06-21 10:45:48 +0800

    [fix] command args

--- framework/core/src/python/kungfu/console/commands/slicetool.py
+++ framework/core/src/python/kungfu/console/commands/slicetool.py
@@ -35,13 +35,13 @@ yjj = kungfu.__binding__.yijinjing
 )
 @click.option(
     "-i",
-    "--indexr_path",
+    "--indexer_path",
     type=str,
     required=False,
     help="path to indexer dynamic library",
 )
 @kfc.pass_context()
-def slicetool(ctx, begin, end, category, group, name, tool_path, index_path):
+def slicetool(ctx, begin, end, category, group, name, tool_path, indexer_path):
     location = yjj.location(
         kfj.MODES["data"],
         kfj.CATEGORIES[category],

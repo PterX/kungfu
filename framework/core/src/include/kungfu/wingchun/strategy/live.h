8e83c07318e54dc14cf5dbf01b9db017af222d39
Wei-Jianan <weij@tcd.ie>
2023-06-24 15:11:09 +0800

    [upd] refactor

--- framework/core/src/include/kungfu/wingchun/strategy/live.h
+++ framework/core/src/include/kungfu/wingchun/strategy/live.h
@@ -263,7 +263,6 @@ private:
   std::unordered_map<uint32_t, uint32_t> account_location_ids_ = {};
   std::unordered_map<std::string, yijinjing::data::location_ptr> market_data_ = {};
 
-  friend void enable(LiveContext &context) { context.on_start(); }
 };
 
 DECLARE_PTR(LiveContext)

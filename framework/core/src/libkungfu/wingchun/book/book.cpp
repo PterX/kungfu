cc12b74f5f2487193c93a4407120b65887eb846f
yizhi <zhangyiizhi@126.com>
2023-08-07 14:40:34 +0800

    [fix] no longer assert for position < 0

--- framework/core/src/libkungfu/wingchun/book/book.cpp
+++ framework/core/src/libkungfu/wingchun/book/book.cpp
@@ -29,10 +29,8 @@ double Book::get_frozen_price(uint64_t order_id) {
 void Book::ensure_position(const InstrumentKey &instrument_key) {
   if (is_shortable(instrument_key.instrument_type)) {
     [[maybe_unused]] const auto &short_position = get_position_for(Direction::Short, instrument_key);
-    assert(short_position.volume >= 0);
   }
   [[maybe_unused]] auto &long_position = get_position_for(Direction::Long, instrument_key);
-  assert(long_position.volume >= 0);
 }
 
 bool Book::has_long_position(const char *exchange_id, const char *instrument_id) const {

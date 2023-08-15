a1962f5551908e75ee0c704b674c82e87b2e4a30
NoTwoBoy <1244476905@qq.com>
2023-08-15 10:18:48 +0800

    [optimize] remove some space of confirm statement in language

--- framework/api/src/language/zh-CN.ts
+++ framework/api/src/language/zh-CN.ts
@@ -76,10 +76,10 @@ export default {
   trading_day: '交易日',
 
   delete_category:
-    '删除 {category} 所有数据,  如果该 {categoryName} 进程正在运行, 也将停止进程, 确认是否删除',
+    '删除{category} 所有数据,  如果该{categoryName}进程正在运行, 也将停止进程, 确认是否删除',
   add_config_modal:
-    '{category} ID 系统唯一, {changeTypeName} 成功后不可修改, 确认是否 {key}',
-  update_config_modal: '确认{key} 相关配置',
+    '{category} ID 系统唯一, {changeTypeName}成功后不可修改, 确认是否{key}',
+  update_config_modal: '确认{key}相关配置',
   database_locked: '检测到当前有交易进行, 数据库被占用',
   export_database_locked:
     '检测到当前有交易进行, 为不影响交易数据落地, 建议收盘后尝试进行此操作',

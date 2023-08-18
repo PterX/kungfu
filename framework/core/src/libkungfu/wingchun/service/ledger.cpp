3da015c643d2b46d193a9e83aa9a161b1d09a966
yizhi <zhangyiizhi@126.com>
2023-08-18 18:48:55 +0800

    [format]

--- framework/core/src/libkungfu/wingchun/service/ledger.cpp
+++ framework/core/src/libkungfu/wingchun/service/ledger.cpp
@@ -262,7 +262,7 @@ void Ledger::rebuild_positions(int64_t trigger_time, uint32_t strategy_uid) {
       position.frozen_yesterday = 0;
       position.open_volume = 0;
       position.static_yesterday_volume = 0;
-      // should keep avg_open_price and position_cost_price 
+      // should keep avg_open_price and position_cost_price
       // position.avg_open_price = 0;
       // position.position_cost_price = 0;
       position.update_time = trigger_time;

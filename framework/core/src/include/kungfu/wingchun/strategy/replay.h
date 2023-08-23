7d7a9ccc9a5a64be3b13545596db2fdee39afc57
Wei-Jianan <weij@tcd.ie>
2023-08-23 13:13:20 +0800

    [format]

--- framework/core/src/include/kungfu/wingchun/strategy/replay.h
+++ framework/core/src/include/kungfu/wingchun/strategy/replay.h
@@ -156,7 +156,8 @@ public:
   uint64_t insert_order_trigger(const std::string &instrument_id, const std::string &exchange_id,
                                 const std::string &source, const std::string &account, double limit_price,
                                 int64_t volume, longfist::enums::PriceType type, longfist::enums::Side side,
-                                longfist::enums::Offset offset, longfist::enums::OrderTriggerType trigger_type, double stop_price = 0,
+                                longfist::enums::Offset offset, longfist::enums::OrderTriggerType trigger_type,
+                                double stop_price = 0,
                                 longfist::enums::HedgeFlag hedge_flag = longfist::enums::HedgeFlag::Speculation,
                                 bool is_swap = false) override;
 

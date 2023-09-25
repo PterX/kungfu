ff3c750c94245dd2dbe044f12eafaa0e04736efc
Wei-Jianan <weij@tcd.ie>
2023-09-25 14:36:58 +0800

    [feat] statData config in backtest

--- framework/core/src/libkungfu/yijinjing/practice/hero.cpp
+++ framework/core/src/libkungfu/yijinjing/practice/hero.cpp
@@ -25,7 +25,8 @@ using namespace kungfu::yijinjing::nanomsg;
 namespace kungfu::yijinjing::practice {
 
 inline std::string encode(const io_device_ptr &io_device) {
-  return fmt::format("{:08x}", io_device->get_live_home()->uid);
+  auto home_uid = io_device->get_home()->mode == mode::BACKTEST ? io_device->get_home()->uid : io_device->get_live_home()->uid;
+  return fmt::format("{:08x}", home_uid);
 }
 
 hero::hero(io_device_ptr io_device)

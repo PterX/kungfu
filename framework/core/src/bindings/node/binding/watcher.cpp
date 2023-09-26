f932b3b04d2ef23d70309b5a3a789da74278c226
wangyuying <ying.yu@kungfu.link>
2023-09-26 10:48:06 +0800

    add issuebasket bind

--- framework/core/src/bindings/node/binding/watcher.cpp
+++ framework/core/src/bindings/node/binding/watcher.cpp
@@ -417,7 +417,9 @@ void Watcher::Init(Napi::Env env, Napi::Object exports) {
                       InstanceMethod("getInstrumentType", &Watcher::GetInstrumentType),                 //
                       InstanceMethod("publishState", &Watcher::PublishState),                           //
                       InstanceMethod("isReadyToInteract", &Watcher::IsReadyToInteract),                 //
-                      InstanceMethod("issueCustomData", &Watcher::IssueCustomData),                     //
+                      InstanceMethod("issueCustomData", &Watcher::IssueCustomData),
+                      InstanceMethod("IssueBasket", &Watcher::IssueBasket),
+                      InstanceMethod("IssueBasketInstrument", &Watcher::IssueBasketInstrument),                     //
                       InstanceMethod("issueBlockMessage", &Watcher::IssueBlockMessage),                 //
                       InstanceMethod("issueOrderTrigger", &Watcher::IssueOrderTrigger),                 //
                       InstanceMethod("issueOrder", &Watcher::IssueOrder),                               //

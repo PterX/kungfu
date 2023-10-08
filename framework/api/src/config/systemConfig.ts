75a02f63c7329aeeeb5511b200d35240c496c186
jickchen34 <jickchen12399@gmail.com>
2023-10-08 14:04:48 +0800

    [fix] pre and suf with chinese

--- framework/api/src/config/systemConfig.ts
+++ framework/api/src/config/systemConfig.ts
@@ -8,4 +8,4 @@ export const SpecialWordsReg = new RegExp(
 export const EnterableSpecialWordsReg = new RegExp('[,.:/]+', 'ig');
 
 export const InvalidFileNameReg =
-  /^(?:[^a-zA-Z0-9_/:？*' '<>$|]|.*[^a-zA-Z0-9_/:？*' '<>$|]$|.*[/:？?*' <>，：｜|$\\].*|\b(?:[cC][oO][nN]|[pP][rR][nN]|[aA][uU][xX]|[nN][uU][lL]|[cC][oO][mM][0-9]|[lL][pP][tT][0-9])\b)/;
+  /^(?:[^a-zA-Z0-9\u4E00-\u9FA5_/:？*' '<>$|]|.*[^a-zA-Z0-9\u4E00-\u9FA5_/:？*' '<>$|]$|.*[/:？?*' <>，：｜|$\\].*|\b(?:[cC][oO][nN]|[pP][rR][nN]|[aA][uU][xX]|[nN][uU][lL]|[cC][oO][mM][0-9]|[lL][pP][tT][0-9])\b)/;

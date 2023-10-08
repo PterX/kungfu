96477fb0b6021d124a962805c5a682e613ce0d97
yizhi <zhangyiizhi@126.com>
2023-10-08 17:31:35 +0800

    [fix] cached storage for app

--- framework/core/src/libkungfu/yijinjing/cache/cached.cpp
+++ framework/core/src/libkungfu/yijinjing/cache/cached.cpp
@@ -255,8 +255,9 @@ void cached::register_trigger_listen_public(int64_t gen_time, const Register &re
   auto app_uid = register_data.location_uid;
   auto app_location = get_location(app_uid);
 
-  if (app_location->category != category::TD and
-      (app_location->group != "node" && app_location->category != category::SYSTEM)) {
+  // pass when not td, or not app (for static info storage)
+  if (app_location->category != category::TD or
+      not(app_location->group == "node" && app_location->category == category::SYSTEM)) {
     return;
   }
 

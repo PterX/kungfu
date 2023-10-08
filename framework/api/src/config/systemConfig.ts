5df130e73509a12026f2e2e9bf9e7791d5cc0e51
jickchen34 <jickchen12399@gmail.com>
2023-10-08 13:30:01 +0800

    [fix] delete EmojiRegex

--- framework/api/src/config/systemConfig.ts
+++ framework/api/src/config/systemConfig.ts
@@ -7,7 +7,5 @@ export const SpecialWordsReg = new RegExp(
 
 export const EnterableSpecialWordsReg = new RegExp('[,.:/]+', 'ig');
 
-export const EmojiRegex = /\p{Emoji}/u;
-
 export const InvalidFileNameReg =
   /^(?:[^a-zA-Z0-9_/:？*' '<>$|]|.*[^a-zA-Z0-9_/:？*' '<>$|]$|.*[/:？?*' <>，：｜|$\\].*|\b(?:[cC][oO][nN]|[pP][rR][nN]|[aA][uU][xX]|[nN][uU][lL]|[cC][oO][mM][0-9]|[lL][pP][tT][0-9])\b)/;

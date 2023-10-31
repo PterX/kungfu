b5b94c2e67b8acfa84ead4e771c3238ea9c4f54f
yizhi <zhangyiizhi@126.com>
2023-10-31 13:46:19 +0800

    [fix] update book after asset

--- framework/core/src/libkungfu/wingchun/book/bookkeeper.cpp
+++ framework/core/src/libkungfu/wingchun/book/bookkeeper.cpp
@@ -74,8 +74,8 @@ void Bookkeeper::on_start(const rx::connectable_observable<event_ptr> &events) {
   events | is(Order::tag) | $$(update_book<Order>(event, &AccountingMethod::apply_order));
   events | is(Trade::tag) | $$(update_book<Trade>(event, &AccountingMethod::apply_trade));
   events | fork<Asset>(location::SYNC, &Bookkeeper::try_sync_asset, &Bookkeeper::try_update_asset);
-  events | is(Asset::tag) | $$(update_book(event, event->data<Asset>()));
   events | fork<AssetMargin>(location::SYNC, &Bookkeeper::try_sync_asset_margin, &Bookkeeper::try_update_asset_margin);
+  events | is(Asset::tag) | $$(update_book(event, event->data<Asset>()));
   events | fork<Position>(location::SYNC, &Bookkeeper::try_sync_position, &Bookkeeper::try_update_position);
   events | fork<PositionEnd>(location::SYNC, &Bookkeeper::try_sync_position_end, &Bookkeeper::try_update_position_end);
   events | is(TradingDay::tag) | $$(on_trading_day(event->data<TradingDay>().timestamp));

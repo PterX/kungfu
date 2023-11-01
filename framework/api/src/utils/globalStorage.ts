1601c12f42ce1df8090eaed7029801d94b4ed0fe
NoTwoBoy <1244476905@qq.com>
2023-11-01 18:04:49 +0800

    Revert "[fix] fix globalStorage unique in global"
    This reverts commit 34c5b88f513b83e317886c51bd42e2eb2dc5bb39.

--- framework/api/src/utils/globalStorage.ts
+++ framework/api/src/utils/globalStorage.ts
@@ -123,7 +123,8 @@ export function getGlobalStorage<T>(): ExtendedGlobalStorage<T> {
         return Reflect.get(target, key);
       },
     });
+    return newGlobalStorage;
+  } else {
+    return globalThis.globalStorage;
   }
-
-  return globalThis.globalStorage;
 }

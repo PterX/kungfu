901bb09d7aa91256646f133ebebd9e4d80f841dc
NoTwoBoy <1244476905@qq.com>
2023-11-01 18:05:12 +0800

    Revert "[fix] remove log"
    This reverts commit 79bdef91d4acd2534f35a6156a2da643eb895823.

--- framework/app/src/renderer/pages/index/main.ts
+++ framework/app/src/renderer/pages/index/main.ts
@@ -239,6 +239,7 @@ const initStartAll = (bypassArchive = false) => {
 loadCustomFont().then(async () => {
   await mergeExtLanguages();
   await useComponents(app, router);
+  console.log('ifTodayFirstStart', ifTodayFirstStart());
   (globalThis.HookKeeper as KfHookKeeper)
     .getHooks()
     .lifeCycle.trigger(LifeCycleKeys.BeforeAppMount)

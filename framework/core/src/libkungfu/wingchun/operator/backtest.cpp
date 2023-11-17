4689e8e25fa9cdb02cdda4b667aa0a267e701a5b
Wei-Jianan <weij@tcd.ie>
2023-11-17 16:11:50 +0800

    [upd] refactor

--- framework/core/src/libkungfu/wingchun/operator/backtest.cpp
+++ framework/core/src/libkungfu/wingchun/operator/backtest.cpp
@@ -138,15 +138,17 @@ void BacktestContext::init_time_events() {
 
 void BacktestContext::subscribe(const std::string &source, const std::vector<std::string> &instrument_ids,
                                 const std::string &exchange_id) {
-  for (auto data_type : {Quote::tag, Tree::tag, Entrust::tag, Transaction::tag}) {
+  boost::hana::for_each(longfist::MarketDataTypes, [&](auto it) {
+    using DataType = typename decltype(+boost::hana::second(it))::type;
+    auto data_type_tag = DataType::tag;
     for (const auto &instrument_id : instrument_ids) {
       int64_t slice_begin_time = app_.now();
       int64_t slice_end_time{INT64_MAX};
       do {
         auto md_location = from_indexer_->find_md_slice_location(slice_begin_time, source, source, instrument_id,
-                                                                 exchange_id, data_type);
+                                                                 exchange_id, data_type_tag);
         slice_end_time = from_indexer_->get_md_slice_end_time(slice_begin_time, source, source, instrument_id,
-                                                              exchange_id, data_type);
+                                                              exchange_id, data_type_tag);
         if (not md_location) 
           continue;
         if (md_location->locator->list_page_id(md_location, location::PUBLIC).empty()) {
@@ -165,7 +167,7 @@ void BacktestContext::subscribe(const std::string &source, const std::vector<std
         lease_locations_[slice_end_time].push_back(std::move(md_location));
       } while ((slice_begin_time = 1 + slice_end_time) < app_.get_end_time());
     }
-  }
+  });
 }
 
 void BacktestContext::unsubscribe(const std::string &source, const std::vector<std::string> &instrument_ids,

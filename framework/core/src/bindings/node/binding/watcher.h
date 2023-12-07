02d839a92af69ddd162389ad2ac9333063a5ee12
yizhi <zhangyiizhi@126.com>
2023-12-07 13:47:20 +0800

    [fix] frozen volume calculation

--- framework/core/src/bindings/node/binding/watcher.h
+++ framework/core/src/bindings/node/binding/watcher.h
@@ -283,7 +283,7 @@ private:
   template <typename TradingData>
   std::enable_if_t<std::is_same_v<TradingData, longfist::types::OrderInput>> UpdateBook(uint32_t source, uint32_t dest,
                                                                                         const TradingData &data) {
-    bookkeeper_.on_order_input(now(), dest, source, data);
+    bookkeeper_.on_order_input(now(), source, dest, data);
     state<kungfu::longfist::types::OrderInput> cache_state_order_input(source, dest, now(), data);
     data_bank_ << cache_state_order_input;
   }

22f2b236b4fbda60e45901610b520c3e2b2ef950
NoTwoBoy <1244476905@qq.com>
2023-12-09 11:28:30 +0800

    [fix] format code

--- framework/app/src/components/modules/pos/utils.ts
+++ framework/app/src/components/modules/pos/utils.ts
@@ -1,6 +1,9 @@
 import { OffsetEnum } from '@kungfu-trader/kungfu-js-api/typings/enums';
 import { getOffsetByOffsetFilter } from '@kungfu-trader/kungfu-js-api/utils/busiUtils';
-import { isShotable, isT0 } from '@kungfu-trader/kungfu-js-api/utils/tradingUtils';
+import {
+  isShotable,
+  isT0,
+} from '@kungfu-trader/kungfu-js-api/utils/tradingUtils';
 
 export const resolveTriggerOffset = (position: KungfuApi.PositionResolved) => {
   const { instrument_type, exchange_id } = position;

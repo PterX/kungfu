7b0434914e04b293731cce6e69935dfcc9b2d2e3
wangqiandong <wqd19960517@163.com>
2024-01-05 21:40:03 +0800

    Modify the way the market_value is processed

--- framework/core/src/libkungfu/wingchun/book/book.cpp
+++ framework/core/src/libkungfu/wingchun/book/book.cpp
@@ -171,19 +171,18 @@ void Book::update(int64_t update_time, longfist::enums::AccountingMethodType acc
 
     if (position.direction == Direction::Long || !is_stock) {
       asset.market_value += position_market_value;
-      asset.unrealized_pnl += position.unrealized_pnl * db_exchage_rate;
+    }
+    asset.unrealized_pnl += position.unrealized_pnl * db_exchage_rate;
 
-      if (is_stock) {
-        asset.dynamic_equity += position_market_value;
-      } else if (is_future) {
-        asset.dynamic_equity += position.margin + position.position_pnl * db_exchage_rate;
-      }
-
-      if (position.direction == Direction::Short) {
-        asset.short_market_value += position_market_value;
-      } else {
-        asset.long_market_value += position_market_value;
-      }
+    if (is_stock) {
+      asset.dynamic_equity += position_market_value;
+    } else if (is_future) {
+      asset.dynamic_equity += position.margin + position.position_pnl * db_exchage_rate;
+    }
+    if (position.direction == Direction::Short) {
+      asset.short_market_value += position_market_value;
+    } else {
+      asset.long_market_value += position_market_value;
     }
   };
 

f923c650afe820e69d1b7a6fae20d18df5553fa3
yizhi <zhangyiizhi@126.com>
2024-01-11 17:22:36 +0800

    [add] watcher refresh ledger mode support keeping unfinished status trading data

--- framework/cli/src/setDzxyEnv.ts
+++ framework/cli/src/setDzxyEnv.ts
@@ -1,4 +1,5 @@
 process.env.APP_ID = 'dzxy';
 process.env.BY_PASS_RESTORE = true;
+process.env.BY_PASS_REFRESHBOOK = true;
 process.env.REFRESH_LEDGER_BEFORE_SYNC = true;
-process.env.MILLISECONDS_SLEEP_AFTER_STEP = 500;
+process.env.MILLISECONDS_SLEEP_AFTER_STEP = 100;

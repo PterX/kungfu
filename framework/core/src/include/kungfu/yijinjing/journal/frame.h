cd0c43f392003ec3bd0ea779d9d7f9515e76b952
kfkev <kevcqu@qq.com>
2024-02-21 21:02:48 +0800

    [modify] nanomsg_json

--- framework/core/src/include/kungfu/yijinjing/journal/frame.h
+++ framework/core/src/include/kungfu/yijinjing/journal/frame.h
@@ -53,7 +53,15 @@ struct frame : event {
 
   [[nodiscard]] std::string data_as_string() const override { return std::string{data_as_bytes(), data_length()}; }
 
-  [[nodiscard]] std::string to_string() const override { return std::string{reinterpret_cast<char *>(address())}; }
+  [[nodiscard]] std::string to_string() const override {
+    auto j = header_->to_json();
+    if (is_json()) {
+      j["data"] = nlohmann::json::parse(data_as_string());
+    } else {
+      j["data"] = data_as_string();
+    }
+    return j.dump(-1, ' ', false, nlohmann::json::basic_json::error_handler_t::replace);
+  }
 
   [[nodiscard]] int8_t data_type() const override { return int8_t(header_->data_type); }
 

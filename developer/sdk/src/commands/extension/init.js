fb24fa89cc4cd29c9827962c38bbc2a183c244e1
jickchen34 <jickchen12399@gmail.com>
2024-03-08 15:31:38 +0800

    [fix] Update extension init description

--- developer/sdk/src/commands/extension/init.js
+++ developer/sdk/src/commands/extension/init.js
@@ -2,7 +2,7 @@ const sdk = require('@kungfu-trader/kungfu-sdk');
 
 module.exports = {
   flags: 'init',
-  desc: 'Init template',
+  desc: 'Init extension project',
   run: () => {
     sdk.lib.extension.init();
   },

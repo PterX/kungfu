d00fe6f199556972c75e317ffb0cfb90b657bcfb
kfkev <kevcqu@qq.com>
2024-03-27 13:59:57 +0800

    [fix] .

--- framework/core/src/include/kungfu/yijinjing/common.h
+++ framework/core/src/include/kungfu/yijinjing/common.h
@@ -339,6 +339,6 @@ std::function<Result(Observable)> holdon() {
 } // namespace rx
 } // namespace kungfu
 
-#define $$(handler) $([&](const kungfu::event_ptr &event) { handler; })
+#define $$(handler) kungfu::rx::$([&](const kungfu::event_ptr &event) { handler; })
 
 #endif // KUNGFU_YIJINJING_COMMON_H

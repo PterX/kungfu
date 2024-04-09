06de146cffdaa3a4ee858d21418c16c988ee0281
yizhi <zhangyiizhi@126.com>
2024-04-09 12:47:21 +0800

    [fix] step lock

--- framework/core/src/bindings/node/binding/watcher.cpp
+++ framework/core/src/bindings/node/binding/watcher.cpp
@@ -523,7 +523,7 @@ void Watcher::Sync(const Napi::CallbackInfo &info) {
   SyncAppStates();
   SyncStrategyStates();
   SyncLedger();
-  TryRefreshTradingData(info);
+  TryRefreshTradingData();
   SyncTradingDataFromCached();
   SyncTradingData();
   ResetTradingDataCount();
@@ -536,12 +536,12 @@ void Watcher::SyncLedger() {
   });
 }
 
-void Watcher::TryRefreshTradingData(const Napi::CallbackInfo &info) {
+void Watcher::TryRefreshTradingData() {
   if (not refresh_trading_data_before_sync_) {
     return;
   }
 
-  serialize::RefreshTradingDataInStateMap(info, ledger_ref_, "ledger");
+  serialize::RefreshTradingDataInStateMap(ledger_ref_, "ledger");
 }
 
 void Watcher::SyncTradingData() {
@@ -717,9 +717,13 @@ void Watcher::StartWorker() {
       if (not watcher->is_live() and not watcher->is_started() and watcher->is_usable()) {
         watcher->setup();
       }
-      while (watcher->is_live() &&
-             (watcher->reader_->data_available() || watcher->get_trading_data_reader()->data_available())) {
+      while (watcher->is_live()) {
         std::lock_guard<std::mutex> guard(watcher->feed_mutex_);
+
+        if (not watcher->is_step_continually()) {
+          break;
+        }
+
         watcher->step(STEP_INTERVAL);
         watcher->drain_from_trading_data_reader(STEP_INTERVAL);
       }
@@ -848,6 +852,13 @@ void Watcher::drain_from_trading_data_reader(uint32_t step_limit) {
     step_count++;
     trading_data_reader_->next();
   }
+}
+
+bool Watcher::is_step_continually() {
+  bool default_reader_available = reader_->data_available();
+  bool trading_data_reader_available = trading_data_reader_->data_available();
+  bool is_read_enough = trading_data_count_by_step_ >= REFRESH_REQUIRED_DATA_LIMIT_BY_SYNC;
+  return default_reader_available || (trading_data_reader_available && not is_read_enough);
 }
 
 void Watcher::UpdateBook(const event_ptr &event, const Position &position) {

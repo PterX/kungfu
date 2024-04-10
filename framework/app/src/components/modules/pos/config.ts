30c21fd1cd5378cbeb299235642192e63ccebb1e
secretlemon <1813933328@qq.com>
2024-04-10 13:43:55 +0800

    [fix] Fixed the copywriting color issue of positions and position summary

--- framework/app/src/components/modules/pos/config.ts
+++ framework/app/src/components/modules/pos/config.ts
@@ -184,7 +184,9 @@ export const getColumns = (
         style: {
           textAlign: 'right',
           color: (args) => {
-            return args.dataValue > 0
+            if (!Number(args.dataValue)) return defaultColorMap['text'];
+
+            return +args.dataValue > 0
               ? defaultColorMap['red']
               : defaultColorMap['green'];
           },

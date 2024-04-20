358b98b33239ee0d251ae6929474393416ba9760
yizhi <zhangyiizhi@126.com>
2024-04-20 23:18:13 +0800

    [fix] upgrade pybind && support python>=3.9,<3.11

--- extensions/xtp/src/cpp/type_convert.h
+++ extensions/xtp/src/cpp/type_convert.h
@@ -355,7 +355,7 @@ inline void from_xtp(const XTPTickByTickStruct &ori, Entrust &des) {
   }
 
   // xtp（深交所的order_no在xtp接口注释标注为无意义，偶尔为0 seq对应的是真正的订单号 上交所的order_no是订单号）
-  if (des.exchange_id == "SSE") {
+  if (strcmp(des.exchange_id, "SSE")) {
     des.orig_order_no = ori.entrust.order_no;
   } else {
     des.orig_order_no = ori.entrust.seq;
--- framework/core/src/python/setup.py
+++ framework/core/src/python/setup.py
@@ -8,13 +8,29 @@ from poetry.core.factory import Factory
 from poetry.core.masonry.builders.sdist import SdistBuilder
 from setuptools import find_packages
 from setuptools import setup
+from setuptools.dist import Distribution
 
 package = Factory().create_poetry(Path(".").resolve()).package
 dependencies, extras = SdistBuilder.convert_dependencies(package, package.requires)
 
+install_requires = []
+white_list = ["click"]
+for r in package.requires:
+    if r.name in white_list:
+        install_requires.append(f"{r.name}=={r.constraint.flatten()[0].min}")
+    else:
+        install_requires.append(f"{r.name}>={r.constraint.flatten()[0].min}")
+
 build_info_path = path.join(path.dirname(__file__), "kungfubuildinfo.json")
 with open(build_info_path, "r") as build_info_file:
     build_info = json.load(build_info_file)
+
+
+class BinaryDistribution(Distribution):
+    """Distribution which always forces a binary package with platform name"""
+
+    def has_ext_modules(self):
+        return True
 
 setup(
     name=package.name,
@@ -24,11 +40,10 @@ setup(
     url=package.homepage,
     project_urls={"repository": package.repository_url},
     packages=[""] + find_packages(exclude=["test"]),
-    package_data={
-        "": ["*.dll", "*.dylib", "*.node", "*.pyd", "*.so", "*.so.*", "*.json"]
-    },
+    package_data={"": ["*.dll", "*.dylib", "*.pyd", "*.so", "*.so.*", "*.json"]},
     include_package_data=True,
-    install_requires=dependencies,
-    # entry_points={"console_scripts": ["kfc = kungfu.__main__:main"]},
+    install_requires=install_requires,
+    entry_points={"console_scripts": ["kfc = kungfu.__main__:main"]},
+    distclass=BinaryDistribution,
     has_ext_modules=lambda: True,
 )

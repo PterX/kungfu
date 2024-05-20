881ea7b5b7c3984358781ea8c55252419b04f2f1
NoTwoBoy <1244476905@qq.com>
2024-05-20 11:20:52 +0800

    [update] add InitialSource for Node

--- framework/core/src/bindings/node/binding/journal.cpp
+++ framework/core/src/bindings/node/binding/journal.cpp
@@ -75,14 +75,15 @@ void Frame::Init(Napi::Env env, Napi::Object exports) {
 
   Napi::Function func = DefineClass(env, "Frame",
                                     {
-                                        InstanceMethod("dataLength", &Frame::DataLength),    //
-                                        InstanceMethod("genTime", &Frame::GenTime),          //
-                                        InstanceMethod("triggerTime", &Frame::TriggerTime),  //
-                                        InstanceMethod("msgType", &Frame::MsgType),          //
-                                        InstanceMethod("source", &Frame::Source),            //
-                                        InstanceMethod("dest", &Frame::Dest),                //
-                                        InstanceMethod("data", &Frame::Data),                //
-                                        InstanceMethod("dataAsString", &Frame::DataAsString) //
+                                        InstanceMethod("dataLength", &Frame::DataLength),       //
+                                        InstanceMethod("genTime", &Frame::GenTime),             //
+                                        InstanceMethod("triggerTime", &Frame::TriggerTime),     //
+                                        InstanceMethod("msgType", &Frame::MsgType),             //
+                                        InstanceMethod("source", &Frame::Source),               //
+                                        InstanceMethod("dest", &Frame::Dest),                   //
+                                        InstanceMethod("initialSource", &Frame::InitialSource), //
+                                        InstanceMethod("data", &Frame::Data),                   //
+                                        InstanceMethod("dataAsString", &Frame::DataAsString)    //
                                     });
 
   constructor = Napi::Persistent(func);

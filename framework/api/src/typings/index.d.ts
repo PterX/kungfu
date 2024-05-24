7721464ac1cd865c0c5957671ce7186251628224
jickchen34 <jickchen12399@gmail.com>
2024-05-24 17:11:05 +0800

    [fix] Remove unused code

--- framework/api/src/typings/index.d.ts
+++ framework/api/src/typings/index.d.ts
@@ -804,7 +804,6 @@ declare namespace KungfuApi {
     status_color: AntInKungfuColorTypes;
     status_resolved: KungfuApi.KfTradeValueCommonData;
     update_time_resolved: string;
-    insert_time_resolved: string;
     limit_price_resolved: string;
   }
 

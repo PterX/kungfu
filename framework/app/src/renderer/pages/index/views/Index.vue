2faa64907a1b34d3692e1aef98ecdb8966f816d9
NoTwoBoy <1244476905@qq.com>
2024-06-06 12:38:53 +0800

    [feat] extract KfBoards component

--- framework/app/src/renderer/pages/index/views/Index.vue
+++ framework/app/src/renderer/pages/index/views/Index.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="kf-index__warp">
-    <KfRowColIter
-      :board-id="0"
+    <KfBoards
+      boards-id="main"
       :closable="true"
-      :init-boards-map="curBoardsMap"
-      :current-boards-store-id="'main'"
-      :default-boards-map="curDefaultBoardsMap"
-    ></KfRowColIter>
+      :boards-map-builder="buildDefaultBoardsMap"
+    ></KfBoards>
     <KfAddBoardModalVue
       v-if="addBoardModalVisible"
       v-model:visible="addBoardModalVisible"
@@ -17,22 +15,21 @@
 
 <script lang="ts">
 import { defineComponent, ref, onActivated, onDeactivated } from 'vue';
+import { Subscription } from 'rxjs';
 
-import KfRowColIter from '@kungfu-trader/kungfu-app/src/renderer/components/layout/KfRowColIter.vue';
+import KfBoards from '@kungfu-trader/kungfu-app/src/renderer/components/layout/KfBoards.vue';
+import KfAddBoardModalVue from '../../../components/public/KfAddBoardModal.vue';
 
 import { useGlobalStore } from '@kungfu-trader/kungfu-app/src/renderer/pages/index/store/global';
 import { defaultBoardsMap } from '@kungfu-trader/kungfu-app/src/renderer/assets/configs';
-import KfAddBoardModalVue from '../../../components/public/KfAddBoardModal.vue';
 import globalBus from '@kungfu-trader/kungfu-js-api/utils/globalBus';
-import { Subscription } from 'rxjs';
-import { useBoards } from '../store/board';
 import { deepClone } from '@kungfu-trader/kungfu-js-api/utils/commonUtils';
 
 export default defineComponent({
   name: 'Index',
 
   components: {
-    KfRowColIter,
+    KfBoards,
     KfAddBoardModalVue,
   },
 
@@ -47,24 +44,16 @@ export default defineComponent({
     const addBoardTargetBoardId = ref<number>(-1);
 
     let subscription: Subscription;
-    const { getLocalBoardsMap } = useBoards();
     const curDefaultBoardsMap = dealDefaultBoardsHook.trigger(
       defaultBoardsMap,
     ) as KfLayout.BoardsMap;
-    const curBoardsMap: KfLayout.BoardsMap =
-      getLocalBoardsMap('main') || deepClone(defaultBoardsMap);
+    const curBoardsMap: KfLayout.BoardsMap = deepClone(curDefaultBoardsMap);
 
     onActivated(() => {
       subscription = globalBus.subscribe((data: KfEvent.KfBusEvent) => {
         if (data.tag === 'addBoard') {
           addBoardModalVisible.value = true;
           addBoardTargetBoardId.value = data.boardId;
-        }
-
-        if (data.tag === 'main') {
-          if (data.name == 'record-before-quit') {
-            window.watcher && window.watcher.quit();
-          }
         }
       });
 
@@ -77,8 +66,7 @@ export default defineComponent({
     });
 
     return {
-      curBoardsMap,
-      curDefaultBoardsMap,
+      buildDefaultBoardsMap: () => curBoardsMap,
       addBoardModalVisible,
       addBoardTargetBoardId,
     };

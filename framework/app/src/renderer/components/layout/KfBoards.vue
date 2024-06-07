4280579cb3906fb6b784aa37af1d86b8d6c9a0ac
NoTwoBoy <1244476905@qq.com>
2024-06-07 16:23:45 +0800

    [fix] fix KfBoards createBoardStore arg

--- framework/app/src/renderer/components/layout/KfBoards.vue
+++ framework/app/src/renderer/components/layout/KfBoards.vue
@@ -24,11 +24,10 @@ const props = defineProps<{
 
 const app = getCurrentInstance();
 const { getLocalBoardsMap, createBoardsStore } = useBoards();
-const curDefaultBoardsMap = props.boardsMapBuilder();
 const curBoardsMap: KfLayout.BoardsMap =
-  getLocalBoardsMap(props.boardsId) || curDefaultBoardsMap;
+  getLocalBoardsMap(props.boardsId) || props.boardsMapBuilder();
 
-createBoardsStore(props.boardsId, curBoardsMap, curDefaultBoardsMap);
+createBoardsStore(props.boardsId, curBoardsMap, props.boardsMapBuilder());
 
 if (app && props.componentsMap) {
   registerComponents(app.appContext.app, props.componentsMap);
